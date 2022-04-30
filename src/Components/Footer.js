c15ddedc7e81154e62fd5bb8b9f2e7f175e12aec
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-04-30 18:50:18 -0300

    Updates

--- src/Components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from '../Footer.module.css'
-
-function Footer() {
-  return <h5 className={ style.footer }>Desenvolvido por Rafael Friedel - 27/04/2022</h5>
-}
-
-export default Footer;
